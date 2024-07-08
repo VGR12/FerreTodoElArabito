aaaf870cef9c2c1c5bbbc3a15e16076ee1c7ebe9
Victor Guillen <victor.guillen@credid.net>
2024-07-08 17:11:11 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FerreTodoElArabito.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
