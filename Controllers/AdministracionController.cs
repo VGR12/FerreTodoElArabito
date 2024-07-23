cdde359dd1bddb49481fc1b9340900d0a029652c
Victor Guillen <victor.guillen@credid.net>
2024-07-23 17:00:50 -0600

    cv

---
+++ Controllers/AdministracionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FerreTodoElArabito.Web.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
