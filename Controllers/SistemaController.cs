1796aeb30c427565f445ac0af4299bcebb1776e6
Victor Guillen <victor.guillen@credid.net>
2024-09-01 19:39:11 -0600

    vista de login

---
+++ Controllers/SistemaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FerreTodoElArabito.Web.Controllers
+{
+    public class SistemaController : Controller
+    {
+        public IActionResult Login(Guid token)
+        {
+            return View();
+        }
+        
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        
+        public IActionResult Recuperar()
+        {
+            return View();
+        }
+
+    }
+}
