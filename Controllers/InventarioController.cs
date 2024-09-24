a456a4537f402da7181f5686b8ded2acbd87e179
Victor Guillen <victor.guillen@credid.net>
2024-09-24 17:06:10 -0600

    cv

---
+++ Controllers/InventarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FerreTodoElArabito.Web.Controllers
+{
+    public class InventarioController : Controller
+    {
+        [HttpGet]
+        [Route("/Agregar")]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/Listar")]
+        public IActionResult Listar()
+        {
+            return View();
+        }
+    }
+}
