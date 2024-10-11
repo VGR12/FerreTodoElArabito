b7d646c02f0a9b509346e2dd4bb5579445f6369e
Victor Guillen <victor.guillen@credid.net>
2024-10-11 14:13:02 -0600

    Orden proyectos

---
+++ Lib/FT.Lib.Data/Database/Contextos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT.Lib.Data.Database
+{
+    public partial class FerreTodosContext : Database.FerreTodo.FerreTodoContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseLazyLoadingProxies();
+                optionsBuilder.UseSqlServer("Data Source=205.209.114.122;Initial Catalog=FerreTodoElArabito;Persist Security Info=True;User ID=ElArabito;Password=?c5ta083L123$;TrustServerCertificate=True");
+            }
+        }
+    }
+}
